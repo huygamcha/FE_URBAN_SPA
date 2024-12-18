4e0ee7975037019aa6ba18ace89d075650fcc4b5
huygamcha <lehuynhhuy2002@gmail.com>
2024-12-19 00:25:55 +0700

    feat: implement banner UI on homepage

---
+++ src/app/data/bannerHome.ts
@@ -0,0 +1,9 @@
+export const bannerHome = [
+  { id: 1, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_6-scaled.jpg' },
+  { id: 2, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_8-scaled.jpg' },
+  { id: 3, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_9-scaled.jpg' },
+  { id: 4, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_10-scaled.jpg' },
+  { id: 5, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_16-scaled.jpg' },
+  { id: 6, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_3_0-scaled.jpg' },
+  { id: 7, image: 'https://urbanoasisspa.vn/wp-content/uploads/2022/07/urban_oasis_spa_5-scaled.jpg' }
+]
