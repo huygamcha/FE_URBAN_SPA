bd89276bf535e16a441d8e82155de3897ab2d974
lhhuyqdn <huy.le@qdn.digital>
2025-04-06 17:54:44 +0700

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -52,21 +52,58 @@ const getAboutUs = async () => {
   }
 }
 
-export const metadata: Metadata = {
-  title: 'UrbanSpa',
-  description:
-    'Bán hàng điện tử, điện thoại, máy tính bảng, khóa học nextjs 14 reactjs typescript pro 2024 by Lập trình thật dễ - Xây dựng website bán hàng',
-  keywords: `LkC`,
-  openGraph: {
-    title: 'UrbanSpa',
+export const generateMetadata = async ({ params }: any): Promise<Metadata> => {
+  const locale = params?.locale // Đặt giá trị mặc định là 'vi'
+
+  return {
+    title: 'Urban Oasis Spa',
     description:
-      'Bán hàng điện tử, điện thoại, máy tính bảng, khóa học nextjs 14 reactjs typescript pro 2024 by Lập trình thật dễ - Xây dựng website bán hàng',
-    type: 'website'
-  },
-  twitter: {
-    title: 'UrbanSpa',
-    description:
-      'Bán hàng điện tử, điện thoại, máy tính bảng, khóa học nextjs 14 reactjs typescript pro 2024 by Lập trình thật dễ - Xây dựng website bán hàng'
+      locale === 'ko'
+        ? '베트남 현지 여행사, 호텔, 투어, 항공권, 픽업 샌딩 차량, 관광지 티켓, 스파 및 마사지 예약을 전문으로 합니다. 여행의 전 과정에 상담을 지원하기도 합니다.'
+        : 'Spa cao cấp, thư giãn và làm đẹp',
+    keywords: locale === 'ko' ? '베트남 현지 여행사, 호텔, 투어' : 'Spa cao cấp, thư giãn và làm đẹp',
+    openGraph: {
+      title: locale === 'ko' ? 'Urban Spa, 베트남 현지 여행사' : 'Urban Spa, Spa cao cấp, thư giãn và làm đẹp',
+      description:
+        locale === 'ko'
+          ? '베트남 현지 여행사, 호텔, 투어, 항공권, 픽업 샌딩 차량, 관광지 티켓, 스파 및 마사지 예약을 전문으로 합니다. 여행의 전 과정에 상담을 지원하기도 합니다.'
+          : 'Spa cao cấp, thư giãn và làm đẹp',
+      type: 'website',
+      url: `https://cdn.kampa.vn`,
+      images: [
+        {
+          url: 'https://cdn.kampa.vn/opengraph.png',
+          width: 1200,
+          height: 630
+        }
+      ]
+    },
+    twitter: {
+      title: locale === 'ko' ? 'Urban Spa, 베트남 현지 여행사' : 'Urban Spa, Spa cao cấp, thư giãn và làm đẹp',
+      description:
+        locale === 'ko'
+          ? '베트남 현지 여행사, 호텔, 투어, 항공권, 픽업 샌딩 차량, 관광지 티켓, 스파 및 마사지 예약을 전문으로 합니다. 여행의 전 과정에 상담을 지원하기도 합니다.'
+          : 'Spa cao cấp, thư giãn và làm đẹp'
+    },
+    icons: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon.svg'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon.svg'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        url: '/favicon.svg'
+      }
+    ]
   }
 }
 
